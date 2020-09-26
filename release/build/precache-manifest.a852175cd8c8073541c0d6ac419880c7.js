02c1d0bbdb112416c2af0667707a4f0354594a4f
normie <38964599+coqui123@users.noreply.github.com>
2020-09-26 02:14:12 +0000

    release added

---
+++ release/build/precache-manifest.a852175cd8c8073541c0d6ac419880c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2b44f3092be10332d1e0d336619e1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ca7d3104f4af2122ad5",
+    "url": "/static/css/main.4eb54492.chunk.css"
+  },
+  {
+    "revision": "4f53af8a89a784f55561",
+    "url": "/static/js/2.02e326bd.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.02e326bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca7d3104f4af2122ad5",
+    "url": "/static/js/main.4d40d239.chunk.js"
+  },
+  {
+    "revision": "d2d665a4ea583e61dbff",
+    "url": "/static/js/runtime-main.9b54cb03.js"
+  }
+]);
